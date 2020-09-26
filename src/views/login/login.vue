2c018ebfb4c2e987c47208577a5c0c708a298276
dongtech <zld126126@126.com>
2020-09-26 19:14:57 +0800

    implement login.vue

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,11 +1,24 @@
 <template>
     <div id="login">
         登录页<el-button>默认按钮</el-button>
+        {{ msg }}
     </div>
 </template>
 <script>
 export default {
-    name: 'login'
+    name: 'login',
+    data(){
+        return {
+            msg: `这是一个普通的登录页~`
+        }
+    },
+    created(){},
+    mounted(){},
+    methods:{
+
+    },
+    props:{},
+    watch:{}
 }
 </script>
 <style lang="scss" scoped>
