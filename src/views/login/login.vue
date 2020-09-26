2832e7d851fb34de1649ecc07c2dfa7a7de232bc
dongtech <zld126126@126.com>
2020-09-26 20:07:14 +0800

    menutab

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,7 +1,10 @@
 <template>
     <div id="login">
-        登录页<el-button>默认按钮</el-button>
-        {{ msg }}
+        <div class="login-wrap">
+            <ul class="menu-tab">
+                <li v-for="item in menuTabs" v-bind:key="item.id" v-bind:class="{'current':item.current}" @click="toggleMenu(item)">{{ item.txt}}</li>
+            </ul>
+        </div>
     </div>
 </template>
 <script>
@@ -9,21 +12,48 @@ export default {
     name: 'login',
     data(){
         return {
-            msg: `这是一个普通的登录页~`
+            menuTabs:[
+                {txt:'登录',current:true},
+                {txt:'注册',current:false}
+            ]
         }
     },
     created(){},
     mounted(){},
     methods:{
-
+        toggleMenu(data){
+            this.menuTabs.forEach(elem => {
+                elem.current = false
+            });
+            data.current = true
+        }
     },
     props:{},
     watch:{}
-}
+} 
 </script>
 <style lang="scss" scoped>
 #login {
     height: 100vh;
     background-color: #344a5f;
+}
+.login-wrap {
+    width: 330px;
+    margin: auto;
+}
+.menu-tab {
+    text-align: center;
+    li {
+        display: inline-block;
+        width: 88px;
+        line-height: 36px;
+        font-size: 14px;
+        color:white;
+        border-radius: 2px;
+        cursor: pointer;
+    }
+    .current {
+        background-color: rgba(0,0,0,0.1);
+    }
 }
 </style>
